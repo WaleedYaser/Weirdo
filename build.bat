93849d968bafecb5f5e0eb9e0ea13fab3ecafb02
WaleedYaser <waleedyaser95@gmail.com>
2020-03-30 00:47:15 +0200

    fix build files

--- build.bat
+++ build.bat
@@ -1,11 +1,13 @@
-REM @echo off
+@echo off
 
 REM TODO(Waleed): find better namae
 set PROJECT_DIR=%~dp0
 set OUTPUT_DIR=%PROJECT_DIR%build\
 
 REM cleanup
-REM rmdir /S /Q %OUTPUT_DIR%
+rmdir /S /Q %OUTPUT_DIR%
 
 call "%PROJECT_DIR%engine\build.bat"
+REM TODO(Waleed): find a way for variable leak
+set PROJECT_DIR=%~dp0
 call "%PROJECT_DIR%playground\build.bat"
--- playground/build.bat
+++ playground/build.bat
@@ -1,4 +1,4 @@
-REM @echo off
+@echo off
 
 set PROJECT_NAME=playground
 set PROJECT_DIR=%~dp0
