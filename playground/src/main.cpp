091a17f55fbfab939473965cb0ed78432ca56325
WaleedYaser <waleedyaser95@gmail.com>
2020-03-31 13:57:51 +0200

    start drawing triangle

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -3,18 +3,36 @@
 
 #include <stdio.h>
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+
 inline static void
 _render_gradient(zero_os_bitmap_t *bitmap, int x_offset, int y_offset)
 {
-	for (int y = 0; y < bitmap->height; ++y)
+	float t1[2] = {float(bitmap->width) / 2.0f, 0.0f};
+	float t2[2] = {0.0f, float(bitmap->height)};
+	float t3[2] = {float(bitmap->width), float(bitmap->height)};
+
+	for (int x = 0; x < bitmap->width; ++x)
 	{
-		for (int x = 0; x < bitmap->width; ++x)
+		for (int y = 0; y < bitmap->height; ++y)
 		{
-			zero_os_color_t c = zero_os_color_t{
-				uint8_t(x + x_offset),
-				uint8_t(y + y_offset),
-				uint8_t(y + y_offset)};
-			zero_os_bitmap_set(bitmap, x, y, c);
+			float v1[2] = {t1[0] - x, t1[1] - y};
+			float v2[2] = {t2[1] - x, t2[1] - y};
+			float v3[2] = {t3[2] - x, t3[1] - y};
+
+			bool b1 = ((v1[0] * v2[1] - v1[1] * v2[0]) >= 0.0f);
+			bool b2 = ((v2[0] * v3[1] - v2[1] * v3[0]) >= 0.0f);
+			bool b3 = ((v3[0] * v1[1] - v3[1] * v1[0]) >= 0.0f);
+
+			zero_os_color_t color;
+			if (b1 && b2 && b3)
+				color = zero_os_color_t{255, 0, 0};
+			else
+				color = zero_os_color_t{};
+
+			bitmap->data[x + y * bitmap->width] = (color.r << 16) | (color.g << 8) | (color.b << 0);
 		}
 	}
 }
@@ -45,13 +63,15 @@ main()
 		y_offset += 10;
 
 		zero_os_microseconds_t frame_time = zero_os_timer_end(timer);
-		zero_os_timer_sleep(50 - uint32_t(frame_time.ms / 1000));
+		if (frame_time.ms / 1000 < 30)
+			zero_os_timer_sleep(uint32_t(30.0f - frame_time.ms / 1000.0f));
 
 		// TODO(Waleed): doesn't work correctly
 		frame_time = zero_os_timer_end(timer);
-		char buffer[256];
-		sprintf_s(buffer, sizeof(buffer), "ms: %.2fms/f\n", frame_time.ms / 1000.0f);
-		OutputDebugString(buffer);
+		printf("ms: %.2fms/f\n", frame_time.ms / 1000.0f);
+	//	char buffer[256];
+	// 	sprintf_s(buffer, sizeof(buffer), "ms: %.2fms/f\n", frame_time.ms / 1000.0f);
+	//	OutputDebugString(buffer);
 
 		timer = zero_os_timer_start();
 	}
